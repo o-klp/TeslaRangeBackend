1dc404ca08839004e3ce4ccb2aab7e2d2df810af
Keenan Lidral-Porter <klp2012@gmail.com>
2015-02-19 12:07:28 -0800

    (feat) adds utility function to check authentication (cookie form Tesla)

--- config/utils.js
+++ config/utils.js
@@ -1,5 +1,19 @@
 var utils = {}
 
+utils.checkAuth = function(req, res, next){
+  if( req.method === "OPTIONS" ){
+    res.status(200).end()
+  }else if( req.path === "/login" ){
+    next()
+  }else{
+    if( req.cookies["user_credentials"] ){
+      next()
+    }else{
+      next(new Error("Please login"))
+    }
+  }
+}
+
 utils.errorHandler = function(err, req, res, next){
   res.status(500).send({ error: err.message })
 }
